b468bce17b6a985c05d64ea812e36552928aa62f
Spencer <bowles7@purdue.edu>
2025-03-27 18:01:45 -0400

    Stop undervolting the system!

--- src/simo.rs
+++ src/simo.rs
@@ -5,7 +5,7 @@ pub struct Simo {}
 impl Simo {
     /// Create a new SIMO peripheral instance.
     pub fn new(simo: crate::pac::Simo, _reg: &mut crate::gcr::GcrRegisters) -> Self {
-        simo.vrego_a().modify(|_, w| unsafe { w.vseta().bits(59) } );
+        simo.vrego_c().modify(|_, w| unsafe { w.vsetc().bits(59) } );
 
         Self {}
     }
